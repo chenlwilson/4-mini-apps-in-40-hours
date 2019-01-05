c5c8384d369a0f047dcd7a5dc2beb123956f5687
Chen Wilson <chenlwilson@gmail.com>
2019-01-05 15:57:01 -0800

    complete MVC refactoring

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -21,20 +21,87 @@ var model = {
 
 /////////////////////////VIEW/////////////////////////////////
 var view = {
+  resetMessage: function() {
+    document.getElementById('gameover').innerHTML = '';
+  },
 
+  drawMessage: function() {
+    document.getElementById('gameover').innerHTML = 'DRAW! GAME OVER!';
+  },
+
+  winMessage: function(player) {
+    var name = document.getElementById('player' + player).value;
+    document.getElementById('gameover').innerHTML = 'WINNER IS PLAYER ' + name + '! GAME OVER!';
+  },
+
+  placePiece: function(e, player) {
+    e.target.innerHTML = player;
+  },
+
+  addTallyCount: function(player) {
+    document.getElementById(player).innerHTML++;
+    console.log(player);
+    console.log(document.getElementById(player).innerHTML);
+  }
 }
-var board = document.getElementById('board');
-board.addEventListener('click', play)
-
-//add event listener to 'new game' button
-var button = document.getElementById('reset');
-button.addEventListener('click', reset);
 
 //gameover message html
 var message = document.getElementById('gameover');
 
 //////////////////////////CONTROLLER///////////////////////////
 var controller = {
+  //addXToPlacement function
+  //when placing X, add 1 to corresponding row, col, diagonal
+  addXToPlacement: function(rowIndex, colIndex) {
+    model.placement.row[rowIndex]++;
+    model.placement.col[colIndex]++;
+    if (rowIndex === colIndex) {
+      model.placement.diagonal[0]++;
+    }
+    if (rowIndex + colIndex === 2) {
+      model.placement.diagonal[1]++;
+    }
+  },
+
+  //addOToPlacement function
+  //when placing O, subtract 1 to corresponding row, col, diagonal
+  addOToPlacement: function(rowIndex, colIndex) {
+    model.placement.row[rowIndex]--;
+    model.placement.col[colIndex]--;
+    if (rowIndex === colIndex) {
+      model.placement.diagonal[0]--;
+    }
+    if (rowIndex + colIndex === 2) {
+      model.placement.diagonal[1]--;
+    }
+  },
+
+  //checkGame function
+  //check if there are 9 placements
+  //if yes, serve up draw message html
+  //check table if there are 3 X or O in a row/col/diag
+  //if yes,
+  //1) serve up winner message html
+  //2) add win count to tally html
+  //once game over, remove event listener on all cells
+  checkGame: function() {
+    if (model.count.X + model.count.O === 9) {
+      view.drawMessage();
+    }
+    if (model.placement.row.indexOf(3) > -1 || model.placement.col.indexOf(3) > -1 || model.placement.diagonal.indexOf(3) > -1) {
+      view.winMessage('X');
+      model.lastWinner = 'X';
+      view.addTallyCount('tallyX')
+      board.removeEventListener('click', controller.play);
+    }
+    if (model.placement.row.indexOf(-3) > -1 || model.placement.col.indexOf(-3) > -1 || model.placement.diagonal.indexOf(-3) > -1) {
+      view.winMessage('O');
+      model.lastWinner = 'O';
+      view.addTallyCount('tallyO')
+      board.removeEventListener('click', controller.play);
+    }
+  },
+
   //reset game function
   //when <button> is clicked,
   //1) clear all X and O on the board
@@ -52,90 +119,48 @@ var controller = {
     model.count.X = 0;
     model.count.O = 0;
 
-    message.innerHTML = '';
-
     model.placement = {
       row: [0, 0, 0],
       col: [0, 0, 0],
       diagonal: [0, 0]
     }
 
-    board.addEventListener('click', play)
-  }
-}
-//play function
-//when click on <td>, place an X or O to the cell
-//then call addToPlacement() to add value to the placement hash table
-//when placement is 5 or more, call checkGame()
-function play(e) {
-  if (!e.target.innerHTML) {
-    var idRow = parseInt(e.target.id.charAt(0));
-    var idCol = parseInt(e.target.id.charAt(1));
-    if (model.lastWinner === 'X' && model.count.X === 0
-    || model.lastWinner === 'X' && model.count.X === model.count.O
-    || model.lastWinner === 'O' && model.count.O - model.count.X === 1) {
-      e.target.innerHTML = 'X';
-      model.count.X++;
-      addXToPlacement(idRow, idCol);
-    } else {
-      e.target.innerHTML = 'O';
-      model.count.O++;
-      addOToPlacement(idRow, idCol);
+    view.resetMessage();
+
+    board.addEventListener('click', controller.play);
+  },
+
+  //play function
+  //when click on <td>, place an X or O to the cell
+  //then call addToPlacement() to add value to the placement hash table
+  //when placement is 5 or more, call checkGame()
+  play: function(e) {
+    if (!e.target.innerHTML) {
+      var idRow = parseInt(e.target.id.charAt(0));
+      var idCol = parseInt(e.target.id.charAt(1));
+      if (model.lastWinner === 'X' && model.count.X === 0
+      || model.lastWinner === 'X' && model.count.X === model.count.O
+      || model.lastWinner === 'O' && model.count.O - model.count.X === 1) {
+        view.placePiece(e, 'X');
+        model.count.X++;
+        controller.addXToPlacement(idRow, idCol);
+      } else {
+        view.placePiece(e, 'O');
+        model.count.O++;
+        controller.addOToPlacement(idRow, idCol);
+      }
+      if (model.count.X + model.count.O >= 5) {
+        controller.checkGame();
+      }
     }
-    if (model.count.X + model.count.O >= 5) {
-      checkGame();
-    }
   }
+
 }
 
-//addXToPlacement function
-//when placing X, add 1 to corresponding row, col, diagonal
-function addXToPlacement(rowIndex, colIndex) {
-  model.placement.row[rowIndex]++;
-  model.placement.col[colIndex]++;
-  if (rowIndex === colIndex) {
-    model.placement.diagonal[0]++;
-    }
-  if (rowIndex + colIndex === 2) {
-    model.placement.diagonal[1]++;
-    }
-}
+//add event listner to the board
+var board = document.getElementById('board');
+board.addEventListener('click', controller.play)
 
-//addOToPlacement function
-//when placing O, subtract 1 to corresponding row, col, diagonal
-function addOToPlacement(rowIndex, colIndex) {
-  model.placement.row[rowIndex]--;
-  model.placement.col[colIndex]--;
-  if (rowIndex === colIndex) {
-    model.placement.diagonal[0]--;
-    }
-  if (rowIndex + colIndex === 2) {
-    model.placement.diagonal[1]--;
-    }
-}
-
-//checkGame function
-//check if there are 9 placements
-//if yes, serve up draw message html
-//check table if there are 3 X or O in a row/col/diag
-//if yes,
-//1) serve up winner message html
-//2) add win count to tally html
-//once game over, remove event listener on all cells
-function checkGame() {
-  if (model.count.X + model.count.O === 9) {
-    message.innerHTML = 'DRAW! GAME OVER!';
-  }
-  if (model.placement.row.indexOf(3) > -1 || model.placement.col.indexOf(3) > -1 || model.placement.diagonal.indexOf(3) > -1) {
-    message.innerHTML = 'WINNER IS X! GAME OVER!';
-    model.lastWinner = 'X';
-    document.getElementById('tallyX').innerHTML++;
-    board.removeEventListener('click', play);
-  }
-  if (model.placement.row.indexOf(-3) > -1 || model.placement.col.indexOf(-3) > -1 || model.placement.diagonal.indexOf(-3) > -1) {
-    message.innerHTML = 'WINNER IS O! GAME OVER!';
-    model.lastWinner = 'O';
-    document.getElementById('tallyO').innerHTML++;
-    board.removeEventListener('click', play);
-  }
-}
+//add event listener to 'new game' button
+var button = document.getElementById('reset');
+button.addEventListener('click', controller.reset);
