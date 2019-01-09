111f5edd09be14722417c629405f0d8673be869c
Chen Wilson <chenlwilson@gmail.com>
2019-01-08 20:43:19 -0800

    added script, can use react and babel

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -2,13 +2,13 @@ class HelloMessage extends React.Component {
   render() {
     return (
       <div>
-        Hello {this.props.name}
+        Hello {this.props.name} !!
       </div>
     );
   }
 }
 
 ReactDOM.render(
-  <HelloMessage name="Taylor" />,
+  <HelloMessage name="Checkout" />,
   document.getElementById('app')
 );
