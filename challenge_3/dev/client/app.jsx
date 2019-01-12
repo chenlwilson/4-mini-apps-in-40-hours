d872b44a5cfccfcf3aa1451d6f47c746085108b1
Chen Wilson <chenlwilson@gmail.com>
2019-01-12 15:56:17 -0800

    working on converting to Redux

--- challenge_3/dev/client/app.jsx
+++ challenge_3/dev/client/app.jsx
@@ -1,3 +1,390 @@
+import React from 'react';
+import Redux from 'redux';
+import { connect } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import Provider from 'react-redux';
+import thunk from 'redux-thunk';
+
+/***************************REDUX*******************************/
+
+///////////////////////////ACTIONS/////////////////////////////
+var changeStep = (step) => {
+  return {
+    type: 'step',
+    step: step
+  }
+}
+
+var changeErr = (err) => {
+  return {
+    type: 'err',
+    err: err
+  }
+}
+
+var changeId = (id) => {
+  return {
+    type: 'id',
+    id: id
+  }
+}
+
+var changeInfo = (info) => {
+  return {
+    type: 'info',
+    info: info
+  }
+}
+
+var getInfo = (id, value) => {
+  return (dispatch, getState) => {
+    let info = getState().info;
+    info[id] = value
+    dispatch(changeInfo(info));
+  }
+}
+
+var showHome = (e) => {
+  e.preventDetaul();
+  return (dispatch) => {
+    dispatch(changeStep('Home'))
+  }
+}
+
+var showF1 = (e) => {
+  e.preventDetaul();
+  return (dispatch) => {
+    dispatch(changeStep('F1'))
+  }
+}
+
+var showF2 = (e) => {
+  e.preventDetaul();
+  return (dispatch, getState) => {
+    let info = getState().info;
+    if (!info.username) {
+      dispatch(changeErr('Missing Username!'))
+    } else if (!info.email) {
+      dispatch(changeErr('Missing Email!'))
+    } else if (!info.password) {
+      dispatch(changeErr('Missing Password!'))
+    } else if (info.username.length < 5) {
+      dispatch(changeErr('Username should be 5 characters or longer.'))
+    } else if (!info.email.includes('@')) {
+      dispatch(changeErr('Invalid Email Address!'))
+    } else if (info.password.length < 6) {
+      dispatch(changeErr('Password should be 6 characters or longer.'))
+    } else {
+      dispatch(changeStep('F2'))
+  }
+}
+
+var showF3 = (e) => {
+  e.preventDetaul();
+  return (dispatch, getState) => {
+    let info = getState().info;
+    if (!info.address1) {
+      dispatch(changeErr('Missing Address!'))
+    } else if (!info.city) {
+      dispatch(changeErr('Missing City Information!'))
+    } else if (!info.state) {
+      dispatch(changeErr('Missing State Information!'))
+    } else if (!info['shipping zip code']) {
+      dispatch(changeErr('Missing Shipping Zip Code!'))
+    } else if (!info.phone) {
+      dispatch(changeErr('Missing Phone Number!'))
+    } else if (info['shipping zip code'].length !== 5) {
+      dispatch(changeErr('Shipping Zip Code should be 5 digits.'))
+    } else if (info.phone.length !== 10) {
+      dispatch(changeErr('Phone Number should be 10 Digits, Numbers Only.'))
+    } else {
+      dispatch(changeStep('F3'))
+    }
+  }
+}
+
+var showSum = (e) => {
+  e.preventDetaul();
+  return (dispatch, getState) => {
+    let info = getState().info;
+    if (!info.username) {
+      dispatch(changeErr('Missing Username!'))
+    } else if (!info.email) {
+      dispatch(changeErr('Missing Email!'))
+    } else if (!info.password) {
+      dispatch(changeErr('Missing Password!'))
+    } else if (info.username.length < 5) {
+      dispatch(changeErr('Username should be 5 characters or longer.'))
+    } else if (!info.email.includes('@')) {
+      dispatch(changeErr('Invalid Email Address!'))
+    } else if (info.password.length < 6) {
+      dispatch(changeErr('Password should be 6 characters or longer.'))
+    } else if (!info.address1) {
+      dispatch(changeErr('Missing Address!'))
+    } else if (!info.city) {
+      dispatch(changeErr('Missing City Information!'))
+    } else if (!info.state) {
+      dispatch(changeErr('Missing State Information!'))
+    } else if (!info['shipping zip code']) {
+      dispatch(changeErr('Missing Shipping Zip Code!'))
+    } else if (!info.phone) {
+      dispatch(changeErr('Missing Phone Number!'))
+    } else if (info['shipping zip code'].length !== 5) {
+      dispatch(changeErr('Shipping Zip Code should be 5 digits.'))
+    } else if (info.phone.length !== 10) {
+      dispatch(changeErr('Phone Number should be 10 Digits, Numbers Only.'))
+    } else if (!info['credit card number']) {
+      dispatch(changeErr('Missing Credit Card Number!'))
+    } else if (!info['expiration date']) {
+      dispatch(changeErr('Missing Expiration Date!'))
+    } else if (!info.cvv) {
+      dispatch(changeErr('Missing CVV!'))
+    } else if (!info['billing zip code']) {
+      dispatch(changeErr('Missing Billing Zip Code!'))
+    } else if (info['credit card number'].length < 15 || info['credit card number'].length > 16) {
+      dispatch(changeErr('Credit Card Number should be 15 or 16 Digits'))
+    } else if (info['expiration date'].length !== 4) {
+      dispatch(changeErr('Expiration Data should be 4 digits, e.g. 0621'))
+    } else if (info.cvv.length !== 3) {
+      dispatch(changeErr('CVV should be 3 digits'))
+    } else if (info['billing zip code'].length !== 5) {
+      dispatch(changeErr('Billing Zip Code should be 5 digits'))
+    } else {
+      dispatch(changeStep('Sum'))
+    }
+  }
+}
+
+var showSumEdit = (e) => {
+  e.preventDetaul();
+  return (dispatch) => {
+    dispatch(changeStep('SumEdit'));
+  }
+}
+
+var showThankYou = (e) => {
+  e.preventDetaul();
+  var infoDefault = {
+    'username': '',
+    'email': '',
+    'password': '',
+    'address1': '',
+    'address2': '',
+    'city': '',
+    'state': '',
+    'shipping zip code' : '',
+    'phone': '',
+    'credit card number': '',
+    'expiration date': '',
+    'cvv': '',
+    'billing zip code': ''
+  }
+
+  return (dispatch, getState) => {
+    let state = getState();
+    sendData({
+      id: state.id,
+      info: state.info
+    })
+    dispatch(changeStep('ThankYou'));
+    dispatch(changeInfo(infoDefault));
+    dispatch(changeId(state.id+1));
+  }
+}
+
+///////////////////////////REDUCERS////////////////////////////
+var changeStepReducer = (state = 'Home', action) => {
+  switch(action.type) {
+    case 'step':
+      return action.step
+    default:
+      return state
+  }
+}
+
+var changeErrReducer = (state = '', action) => {
+  switch(action.type) {
+    case 'err':
+      return action.err
+    default:
+      return state
+  }
+}
+
+var changeIdReducer = (state = 0, action) => {
+  switch(action.type) {
+    case 'id':
+      return action.id
+    default:
+      return state
+  }
+}
+
+var changeInfoReducer = (action) => {
+  switch(action.type) {
+    case 'info':
+      return action.info
+    default:
+      return {
+        'username': '',
+        'email': '',
+        'password': '',
+        'address1': '',
+        'address2': '',
+        'city': '',
+        'state': '',
+        'shipping zip code' : '',
+        'phone': '',
+        'credit card number': '',
+        'expiration date': '',
+        'cvv': '',
+        'billing zip code': ''
+      }
+  }
+}
+
+/////////////////////ROOT REDUCERS//////////////////////////////
+var rootReducer = combineReducers({
+  changeStep: changeStepReducer,
+  changeErr: changeErrReducer,
+  changeId: changeIdReducer,
+  changeInfo: changeInfoReducer
+})
+
+//////////////////////////STORE/////////////////////////////////
+const store = createStore(
+  roodReducer,
+  applyMiddleware(thunk)
+)
+
+/////////////////////////CONTAINERS/////////////////////////////
+/////////HOME///////////
+const mapDispatchToPropsHome = (dispatch) => {
+  return {
+    showF1: (e) => {
+      dispatch(showF1(e))
+    }
+  }
+}
+var HomeContainer = connect(null, mapDispatchToPropsHome)(Home)
+
+////////////F1//////////////
+const mapStateToPropsF1 = (state) => {
+  return {
+    err: state.changeErr,
+    info: state.changeInfo
+  }
+}
+const mapDispatchToPropsF1 = (dispatch) => {
+  return {
+    getInfo: (id, value) => {
+      dispatch(getInfo(id, value))
+    },
+    showF2: (e) => {
+      dispatch(showF2(e))
+    },
+    showHome: (e) => {
+      dispatch(showHome(e))
+    }
+  }
+}
+var F1Container = connect(mapStateToPropsF1, mapDispatchToPropsF1)(F1)
+
+//////////F2/////////////
+const mapStateToPropsF2 = (state) => {
+  return {
+    err: state.changeErr,
+    info: state.changeInfo
+  }
+}
+const mapDispatchToPropsF2 = (dispatch) => {
+  return {
+    getInfo: (id, value) => {
+      dispatch(getInfo(id, value))
+    },
+    showF1: (e) => {
+      dispatch(showF1(e))
+    },
+    showF3: (e) => {
+      dispatch(showF3(e))
+    }
+  }
+}
+var F2Container = connect(mapStateToPropsF2, mapDispatchToPropsF2)(F2)
+
+//////////F3/////////////
+const mapStateToPropsF3 = (state) => {
+  return {
+    err: state.changeErr,
+    info: state.changeInfo
+  }
+}
+const mapDispatchToPropsF3 = (dispatch) => {
+  return {
+    getInfo: (id, value) => {
+      dispatch(getInfo(id, value))
+    },
+    showF2: (e) => {
+      dispatch(showF2(e))
+    },
+    showSum: (e) => {
+      dispatch(showSum(e))
+    }
+  }
+}
+var F3Container = connect(mapStateToPropsF3, mapDispatchToPropsF3)(F3)
+
+//////////Sum/////////////
+const mapStateToPropsSum = (state) => {
+  return {
+    info: state.changeInfo
+  }
+}
+const mapDispatchToPropsSum = (dispatch) => {
+  return {
+    showSumEdit: (e) => {
+      dispatch(showSumEdit(e))
+    },
+    showThankYou: (e) => {
+      dispatch(showThankYou(e))
+    }
+  }
+}
+var SumContainer = connect(mapStateToPropsSum, mapDispatchToPropsSum)(Sum)
+
+//////////SUMEDIT/////////////
+const mapStateToPropsSumEdit = (state) => {
+  return {
+    err: state.changeErr,
+    info: state.changeInfo
+  }
+}
+const mapDispatchToPropsSumEdit = (dispatch) => {
+  return {
+    getInfo: (id, value) => {
+      dispatch(getInfo(id, value))
+    },
+    showF2: (e) => {
+      dispatch(showF2(e))
+    },
+    showSum: (e) => {
+      dispatch(showSum(e))
+    }
+  }
+}
+var SumEditContainer = connect(mapStateToPropsSumEdit, mapDispatchToPropsSumEdit)(SumEdit)
+
+//////////THANKYOU/////////////
+const mapDispatchToPropsThankYou = (dispatch) => {
+  return {
+    showHome: (e) => {
+      dispatch(showHome(e))
+    }
+  }
+}
+var ThankYouContainer = connect(null, mapDispatchToPropsThankYou)(ThankYou)
+/***************************REACT*******************************/
+
 //////////////////////GRANDCHILD COMPONENTS////////////////////
 var InfoLabel = (props) => (
   <div>
@@ -496,7 +883,7 @@ class App extends React.Component {
   }
 }
 
-// ////////////////////////CLIENT/////////////////////////////
+//***************************CLIENT*******************************/
 //using fetch to handle API calls
 //client post request
 var sendData = (data) => {
@@ -529,13 +916,10 @@ var getId = () => {
     .catch(err => console.log('error: ' + err))
 }
 
-//pass lib functions to DOM rendering
-window.sendData = sendData;
-window.getId = getId;
-//local storage to store current step
-
-////////////////////////DOM//////////////////////////////
+//***************************DOM*******************************/
 ReactDOM.render(
-  <App sendData = { window.sendData } getId = { window.getId } myStorage = { window.localStorage }/>,
+  <Provider store = {store}>
+    <App />
+  </Provider>,
   document.getElementById('app')
 );
