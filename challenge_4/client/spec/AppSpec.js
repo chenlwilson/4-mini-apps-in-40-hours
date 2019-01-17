5df87e473c1a609cc44a5296a316fdf70e762efc
Chen Wilson <chenlwilson@gmail.com>
2019-01-16 21:43:56 -0800

    react test utils simulate click does not update state

--- challenge_4/client/spec/AppSpec.js
+++ challenge_4/client/spec/AppSpec.js
@@ -1,6 +1,7 @@
 //libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactTestUtils from 'react-dom/test-utils';
 import sinon from "sinon";
 import { configure, shallow, mount } from 'enzyme';
 import { expect } from 'chai';
@@ -8,18 +9,51 @@ import Adapter from 'enzyme-adapter-react-16'
 
 //components
 import App from '../src/components/App.js';
+import Board from '../src/components/Board.js'
+import GameOver from '../src/components/GameOver.js'
 import TopRow from '../src/components/TopRow.js'
 
 //config
 configure({ adapter: new Adapter() });
 
 describe('Connect-Four component testing', function() {
+  var {
+    Simulate,
+    renderIntoDocument,
+    findRenderedDOMComponentWithClass,
+    scryRenderedDOMComponentsWithClass,
+    scryRenderedDOMComponentsWithTag,
+    findRenderedDOMComponentsWithTag
+  } = ReactTestUtils;
+
+  let app;
+
+  beforeEach(function() {
+    app = renderIntoDocument(
+      <App />
+    );
+  });
+
+  //using react test utils
+  it('should be a stateful class component', function() {
+    expect(React.Component.isPrototypeOf(App)).to.be.true;
+  });
+
+  //using enzyme shallow
   it('should render a dummy test div', function() {
     const wrapper = shallow(<App />);
     const testdiv = <div className='test'></div>;
     expect(wrapper.contains(testdiv)).to.equal(true);
   });
 
+  //using enzyme shallow
+  it('should render a single Board component and a single GameOver component', function() {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(GameOver).length).to.equal(1);
+    expect(wrapper.find(Board).length).to.equal(1);
+  });
+
+  //using enzyme mount + sinon spy
   it('should call play upon top row click', function() {
     const spy = sinon.spy();
     const wrapper = mount(<TopRow play={spy} />);
@@ -27,17 +61,34 @@ describe('Connect-Four component testing', function() {
     expect(spy.calledOnce).to.equal(true);
   });
 
+  ////////////////////////Not Working//////////////////////////////////
+  it('should update play count when top row is clicked', function() {
+    expect(React.Component.isPrototypeOf(App)).to.be.true;
+
+    var toprow = scryRenderedDOMComponentsWithTag(app, 'tr');
+
+    Simulate.click(toprow[0])
+    //console.log(app)
+    Simulate.click(toprow[0])
+    //console.log(app)
+
+    var whiteCells = findRenderedDOMComponentWithClass(app, 'red')
+
+    expect(whiteCells.length).to.equal(40);
+  });
+
   it('should show message for a tie', function() {
-    const wrapper = shallow(<App />);
-    //const message = 'Game Over! Long Game, and it\'s a tie ¯\_(ツ)_/¯';
-    const message = 'Click On The Top Row To Play';
-    wrapper.setState({ count: 10 });
+    const wrapper = mount(<App />);
+    const message = 'Game Over! Long Game, and it\'s a tie ¯\_(ツ)_/¯';
+    //const message = 'Click On The Top Row To Play';
+    wrapper.setState({ count: 42 });
+    console.log(wrapper);
     //expect(wrapper.contains(messageDiv)).to.equal(true);
     expect(wrapper.state('message')).to.equal(message);;
   });
 
   it('should show message for red wins horizontally', function() {
-    const wrapper = shallow(<App />);
+    const wrapper = mount(<App />);
     const message = 'Game Over! red won!'
     wrapper.setState({ h: [0,4,0,0,0,0] });
     expect(wrapper.state('currentPlayer')).to.equal('red');;
@@ -51,16 +102,16 @@ describe('Connect-Four component testing', function() {
   });
 
   it('should show message for red wins diagonally', function() {
-    const wrapper = shallow(<App />);
+    const wrapper = mount(<App />);
     const message = 'Game Over! red won!'
-    wrapper.setState({ h: [0,4,0,0,0,0] });
+    wrapper.setState({ d: [0,4,0,0,0,0] });
     expect(wrapper.state('message')).to.equal(message);;
   });
 
   it('should show message for black wins counter-diagonally', function() {
-    const wrapper = shallow(<App />);
-    const message = 'Game Over! red won!'
-    wrapper.setState({ h: [0,4,0,0,0,0] });
+    const wrapper = mount(<App />);
+    const message = 'Game Over! black won!'
+    wrapper.setState({ cd: [0,-4,0,0,0,0,0] });
     expect(wrapper.state('message')).to.equal(message);;
   });
 
